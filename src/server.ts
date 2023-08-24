d79fcff2aac6c27d8b350a9e66138d0495995ae1
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2023-08-25 02:32:57 +0530

    created server on port 3000

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server is running on the port ${port}`);
+})
