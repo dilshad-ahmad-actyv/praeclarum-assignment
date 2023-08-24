1f1116a22e68b0cbcdc172b02cecc744d96ba11c
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2023-08-25 02:24:03 +0530

    created schema and updated tsconfig.json configurations

---
+++ src/entities/todo.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsBoolean, IsNotEmpty} from 'class-validator';
+@Entity({name: 'todos'})
+export class Todo extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    @IsNotEmpty({ message: "Title is required" })
+    title: string;
+
+    @Column()
+    @IsNotEmpty({ message: "Description is required" })
+    description: string;
+
+    @Column()
+    @IsBoolean({ message: "Status must be a boolean value" })
+    status: boolean;
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+}
