62b15ee87a5e037f9f82f3e70e571467577a73fa
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2023-08-25 02:59:44 +0530

    added database connection with query

--- src/connection/db.connection.ts
+++ src/connection/db.connection.ts
@@ -1,14 +1,17 @@
 import { DataSource } from "typeorm";
+import dotenv from 'dotenv';
+import { Todo } from "../entities/todo.entity";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "postgres",
-    password: "password",
-    database: "typescript_psql_crud",
+    username: process.env.PSQL_USERNAME,
+    password: process.env.PSQL_PASSWORD,
+    database: process.env.PSQL_DATABASE,
     entities: [
-       
+       Todo
     ],
 
 })
